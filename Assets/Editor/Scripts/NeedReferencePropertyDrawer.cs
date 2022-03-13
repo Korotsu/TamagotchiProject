b0df25d03506af9cb740bb05804f27b2835f500c
wil <william.nardone@hotmail.fr>
2022-03-13 08:31:03 +0100

    +Create the HumorManager/Mood/MoodInfluencer responsible for the management of moods; ~Add the newly created features to the custom propertyDrawer and improve overall of the propertydrawer;

--- Assets/Editor/Scripts/NeedReferencePropertyDrawer.cs
+++ Assets/Editor/Scripts/NeedReferencePropertyDrawer.cs
@@ -19,31 +19,33 @@ namespace Tamagotchi
             EditorGUI.indentLevel = 0;
 
             Manager managerValue = property.serializedObject.targetObject as Manager;
-            List<Need> needs = managerValue.tamagotchiManager.needs;
 
-            Rect impactedNeedRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
-
-            SerializedProperty selected = property.FindPropertyRelative("selected");
-            SerializedProperty needName = property.FindPropertyRelative("needName");
-            List<string> options = needs.Select(need => need.name).ToList();
-
-            int previousValue = selected.intValue;
-            selected.intValue = EditorGUI.Popup(impactedNeedRect, selected.intValue, options.ToArray());
-
-            if (selected.intValue != -1)
+            if (managerValue.tamagotchiManager != null)
             {
-                string newName = needs[selected.intValue].name;
+                List<Need> needs = managerValue.tamagotchiManager.needs;
 
-                if (selected.intValue == previousValue && needName.stringValue != newName)
-                    selected.intValue = needs.FindIndex(need => need.name == needName.stringValue);
+                Rect impactedNeedRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+
+                SerializedProperty selected = property.FindPropertyRelative("selected");
+                SerializedProperty needName = property.FindPropertyRelative("needName");
+                List<string> options = needs.Select(need => need.name).ToList();
+
+                int previousValue = selected.intValue;
+                selected.intValue = EditorGUI.Popup(impactedNeedRect, selected.intValue, options.ToArray());
+
+                if (selected.intValue != -1)
+                {
+                    string newName = needs[selected.intValue].name;
+
+                    if (selected.intValue == previousValue && needName.stringValue != newName)
+                        selected.intValue = needs.FindIndex(need => need.name == needName.stringValue);
+                    else
+                        needName.stringValue = needs[selected.intValue].name;
+                }
+
                 else
-                    needName.stringValue = needs[selected.intValue].name;
+                    needName.stringValue = "";
             }
-
-            else
-                needName.stringValue = "";
-
-
 
             EditorGUI.indentLevel = indent;
 
